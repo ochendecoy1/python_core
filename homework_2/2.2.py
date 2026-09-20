d34f5ea31adc699c89072bf848ff5261fbffe9f8
decoy <ecoccoce@gmail.com>
2026-09-20 16:58:01 +0300

    Homework2 (1 pt2)

---
+++ homework_2/2.2.py
@@ -0,0 +1,6 @@
+ # Создайте список и извлеките из него с помощью среза второй элемент
+
+cars = ['audi', 'bmw', 'mersedes', 'geely', 'jaecoo', 'toyota']
+
+second_element = cars[1]
+print(second_element)
